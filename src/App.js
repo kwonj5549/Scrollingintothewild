ec74dc52c1e3fe0de82fed933609fdb11e6604b4
kwill5800 <kwill5800@gmail.com>
2023-12-05 07:05:38 -0800

    second commit

--- src/App.js
+++ src/App.js
@@ -1,37 +1,66 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import './App.css';
 
-
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
-    const mainTextRef = React.useRef(null);
-  useEffect(() => {
-    const handleWheel = (event) => {
-      // Move up when scrolling down, and vice versa
-      gsap.to(mainTextRef.current, {
-        y: "-=100", // Adjust this value to control the speed of the movement
-        ease: "none",
-      });
-    };
+    const mainTextRef = useRef(null);
+    const roadRef = useRef(null);
+    const stripesRef = useRef(null);
+    useEffect(() => {
+        gsap.to(roadRef.current, {
+            x: "100%",
+            ease: "none",
+            scrollTrigger: {
+                trigger: ".App",
+                start: "top top",
+                end: "bottom bottom",
+                scrub: true,
+            }
+        });
+    }, []);
 
-    window.addEventListener("wheel", handleWheel);
+    useEffect(() => {
+        const handleWheel = (event) => {
+            // Move up when scrolling down, and vice versa
+            gsap.to(mainTextRef.current, {
+                y: "-=100", // Adjust this value to control the speed of the movement
+                ease: "none",
+            });
+        };
 
-    return () => {
-      window.removeEventListener("wheel", handleWheel);
-    };
-  }, []);
+        window.addEventListener("wheel", handleWheel);
 
-
-  return (
-      <div className="App">
-        <div className="main-text" ref={mainTextRef}>
-          <p>Scrolling Into the Wild</p>
+        return () => {
+            window.removeEventListener("wheel", handleWheel);
+        };
+    }, []);
+    useEffect(() => {
+        ScrollTrigger.create({
+            animation: gsap.to(stripesRef.current, {
+                backgroundPosition: '100% 0', // Move the background to simulate the stripe movement
+                ease: 'none',
+                repeat: -1, // Infinite loop
+                duration: 1, // Speed of the animation
+            }),
+            trigger: '#road',
+            start: 'top bottom', // Start the animation when the top of the road hits the bottom of the viewport
+            end: 'bottom top', // End the animation when the bottom of the road leaves the top of the viewport
+            scrub: true, // Link the animation progress to the scroll position
+        });
+    }, []);
+    return (
+        <div className="App">
+            <div className="main-text" ref={mainTextRef}>
+                <p>Scrolling Into the Wild</p>
+            </div>
+            <div id="road" >
+                <div id="stripes" ref={stripesRef}></div>
+            </div>
         </div>
-        <div className="road"></div>
-      </div>
-  );
+    );
 }
 
 export default App;
