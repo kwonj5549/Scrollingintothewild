04b6e000f77c4393bcefb5e5b82ab1707379f338
kwill5800 <kwill5800@gmail.com>
2023-12-05 20:31:29 -0800

    Added new animations and styles for mile markers in App.js and App.css
    The App.js and App.css files have been updated to improve the user interface. This change includes adding new images for mile markers and signs, and adding animations to these new elements.
    
    New animation functions `animateMileMarker0` and `animateMileSign` have been added, controlled by the scrolling behavior, for improving visual interaction. They are integrated into the existing event listener for the scrolling event.
    
    In the CSS file, positioning and size specifications for these new elements have been added, ensuring proper placement and visibility. The changes aim to make the graphics more interactive and visually appealing.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import { gsap } from 'gsap';
 import './App.css';
 import carImage from './assets/car.png';
 import backgroundImage from './assets/background.png';
-
+import mileMarker0 from './assets/mile0.png';
+import mileSign from './assets/hanging-sign.png';
 const App = () => {
     const mainTextRef = useRef(null);
     const carRef = useRef(null);
     const stripesRef = useRef(null);
     const animationCurrent = useRef(0);
-
+    const mileMarker0Ref = useRef(null);
+    const mileSignRef = useRef(null);
     const animateText = (y) => {
         gsap.to(mainTextRef.current, {
             y: -y,
@@ -23,37 +25,54 @@ const App = () => {
             ease: "none",
         });
     };
-
+    const animateMileMarker0 = (x) => {
+        gsap.to(mileMarker0Ref.current, {
+            x: -x,
+            ease: "none",
+        });
+    }
     const animateStripes = (x) => {
         gsap.to(stripesRef.current, {
             backgroundPosition: `${-x}px 0`,
             ease: "none",
         });
     };
+    const animateMileSign = (y) => {
+        gsap.to(mileSignRef.current, {
+            y: y,
+            ease: "none",
 
+        });
+    }
+    const handleWheel = (event) => {
+        // Prevent the default scroll behavior
+        event.preventDefault();
+
+        // Update the current animation value
+        animationCurrent.current += event.deltaY;
+        // Clamp the value to not go below 0
+        if (animationCurrent.current < 0) {
+            animationCurrent.current = 0;
+        }
+
+        // Invoke animation functions
+        animateText(animationCurrent.current);
+
+        if (animationCurrent.current < window.innerWidth * 0.40) {
+            // The car should only move to a maximum of 40% of the viewport width
+            animateCar(animationCurrent.current);
+        } else {
+            // Once the car stops, start moving the stripes
+            animateStripes(animationCurrent.current - (window.innerWidth * 0.40));
+            animateMileMarker0(animationCurrent.current - (window.innerWidth * 0.40));
+
+        }
+        if(animationCurrent.current < window.innerHeight * 0.31) {
+            animateMileSign(animationCurrent.current);
+        }
+    };
     useEffect(() => {
-        const handleWheel = (event) => {
-            // Prevent the default scroll behavior
-            event.preventDefault();
 
-            // Update the current animation value
-            animationCurrent.current += event.deltaY;
-            // Clamp the value to not go below 0
-            if (animationCurrent.current < 0) {
-                animationCurrent.current = 0;
-            }
-
-            // Invoke animation functions
-            animateText(animationCurrent.current);
-
-            if (animationCurrent.current < window.innerWidth * 0.40) {
-                // The car should only move to a maximum of 40% of the viewport width
-                animateCar(animationCurrent.current);
-            } else {
-                // Once the car stops, start moving the stripes
-                animateStripes(animationCurrent.current - (window.innerWidth * 0.40));
-            }
-        };
 
         window.addEventListener("wheel", handleWheel, { passive: false });
 
@@ -68,6 +87,12 @@ const App = () => {
                 <p>Scrolling Into the Wild</p>
             </div>
             <div id="grass"></div>
+            <div>
+                <img src={mileSign} alt="Mile Sign" id="mile-sign" ref={mileSignRef}/>
+            </div>
+            <div>
+                <img src={mileMarker0} alt="Mile Marker 0" id="mile-marker-0" ref={mileMarker0Ref}/>
+            </div>
             <div id="road">
                 <div id="stripes" ref={stripesRef}></div>
                 <img src={carImage} alt="Chris's Car" id="car" ref={carRef}/>
