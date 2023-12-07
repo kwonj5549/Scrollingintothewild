a64204191902e77247a05a89ef444dd5678335f3
kwill5800 <kwill5800@gmail.com>
2023-12-07 10:49:32 -0800

    Add text display animation & adjust mile marker animation in App
    Changes introduced in App.js include addition of a new animation function called animateTextDisplay1 along with enhancements to the existing animation functions for better responsiveness. AnimateTextDisplay1 is designed to animate the movement of the text overlay which appears as the user scrolls.
    
    In addition, the animation multiplier has been increased to enhance user experience by making the transitions smoother.
    
    In App.css, positioning and size changes are made to align with the new elements and animations.
    
    These changes enhance the overall look and feel of the application, leading to improved user experience.

--- src/App.js
+++ src/App.js
@@ -1,58 +1,68 @@
-import React, { useEffect, useRef,useState } from 'react';
-import { gsap } from 'gsap';
+import React, {useEffect, useRef, useState} from 'react';
+import {gsap} from 'gsap';
 import './App.css';
 import carImage from './assets/car.png';
 import backgroundImage from './assets/background.png';
 import mileMarker0 from './assets/mile0.png';
 import mileSign from './assets/hanging-sign.png';
+import textDisplay1 from './assets/text_display.png';
+
 const App = () => {
     const mainTextRef = useRef(null);
     const carRef = useRef(null);
     const stripesRef = useRef(null);
     const animationCurrent = useRef(0);
-    const animationPrevious = useRef(0);
+
     const mileMarker0Ref = useRef(null);
     const mileSignRef = useRef(null);
     const [miles, setMiles] = useState(0);
     const animationMiles = useRef(0);
-    const animationMultiplier = 15;
+    const animationMultiplier = 100;
+    const textDisplay1Ref = useRef(null);
     const animateText = (y) => {
         gsap.to(mainTextRef.current, {
-            y: -y*animationMultiplier,
+            y: -y * animationMultiplier,
             ease: "none",
         });
     };
 
     const animateCar = (x) => {
         gsap.to(carRef.current, {
-            x: x*animationMultiplier,
+            x: x * animationMultiplier,
             ease: "none",
         });
     };
     const animateMileMarker0 = (x) => {
         gsap.to(mileMarker0Ref.current, {
-            x: -x*animationMultiplier,
+            x: -x * animationMultiplier,
             ease: "none",
         });
     }
     const animateStripes = (x) => {
         gsap.to(stripesRef.current, {
-            backgroundPosition: `${-x*animationMultiplier}px 0`,
+            backgroundPosition: `${-x * animationMultiplier}px 0`,
             ease: "none",
         });
     };
     const animateMileSign = (y) => {
         gsap.to(mileSignRef.current, {
-            y: animationMultiplier*y,
+            y: animationMultiplier * y,
             ease: "none",
 
         });
 
 
     }
-    const animateMileSignText= (y) => {
+    const animateMileSignText = (y) => {
         // Calculate miles based on the scroll amount
         setMiles(Math.floor(y));
+    }
+    const animateTextDisplay1 = (y) => {
+        gsap.to(textDisplay1Ref.current, {
+            y: animationMultiplier * y,
+            ease: "none",
+
+        });
     }
     const handleWheel = (event) => {
         event.preventDefault();
@@ -74,24 +84,48 @@ const App = () => {
             animateMileSignText(0);
         } else {
             animateCar(carMoveThreshold); // Adjusting car's final position
-            const extraScroll = (animationCurrent.current - carMoveThreshold) ;
+            const extraScroll = (animationCurrent.current - carMoveThreshold);
             animateStripes(extraScroll);
             animateMileMarker0(extraScroll);
             animationMiles.current += scrollIncrement;
             animateMileSignText(extraScroll);
         }
 
-        const mileSignThreshold = window.innerWidth * 0.12 / animationMultiplier;
+        const mileSignThreshold = window.innerHeight * 0.12 / animationMultiplier;
         if (animationCurrent.current < mileSignThreshold) {
             animateMileSign(animationCurrent.current);
         } else {
             animateMileSign(mileSignThreshold); // Adjusting mile sign's final position
         }
+        const textDisplayStart= 10;
+        const textDisplayEnd= 60;
+        const textDisplayThreshold1 = window.innerHeight* 0.39 / animationMultiplier+textDisplayStart;
+        if (animationCurrent.current >= textDisplayStart && animationCurrent.current <= textDisplayEnd) {
+
+            if (animationCurrent.current <= textDisplayThreshold1) {
+                // Move text display down until it reaches the movement threshold
+                animateTextDisplay1(animationCurrent.current - textDisplayStart);
+
+            } else if (animationCurrent.current > textDisplayThreshold1) {
+                // Keep the text display stationary once it reaches the movement threshold
+                animateTextDisplay1(textDisplayThreshold1 - textDisplayStart);
+
+            }
+        } else if (animationCurrent.current > textDisplayEnd) {
+            // Calculate the offset for moving the text display back up
+            const offset = animationCurrent.current - textDisplayEnd;
+            // Ensure the text display moves back up and off the screen
+            const newYPosition = (textDisplayThreshold1 - textDisplayStart) - offset/4;
+            animateTextDisplay1(newYPosition);
+        }else{
+            animateTextDisplay1(0);
+        }
+
     };
     useEffect(() => {
 
 
-        window.addEventListener("wheel", handleWheel, { passive: false });
+        window.addEventListener("wheel", handleWheel, {passive: false});
 
         return () => {
             window.removeEventListener("wheel", handleWheel);
@@ -99,21 +133,32 @@ const App = () => {
     }, []);
 
     return (
-        <div className="App" style={{ backgroundImage: `url(${backgroundImage})` }}>
+        <div className="App" style={{backgroundImage: `url(${backgroundImage})`}}>
             <div className="main-text" ref={mainTextRef}>
                 <p>Scrolling Into the Wild</p>
             </div>
+
+            <div id="mile-sign" ref={mileSignRef}>
+                <img src={mileSign} alt="Mile Sign"/>
+                <div className="milesign-text">
+                    <p>{miles} miles</p>
+                </div>
+            </div>
+            <div className="text-display-container" ref={textDisplay1Ref}>
+                <img src={textDisplay1} id="text-display-1" alt="Text Display 1"/>
+                <div className="display-text">
+                    <p>Chris McCandless begins his journey in the </p>
+                </div>
+            </div>
+
+
+
+
+
+
             <div id="grass">
                 <img src={mileMarker0} alt="Mile Marker 0" id="mile-marker-0" ref={mileMarker0Ref}/>
             </div>
-            <div id="mile-sign" ref={mileSignRef}>
-                <img src={mileSign} alt="Mile Sign"/>
-            <div className="milesign-text" >
-                <p>{miles} miles</p>
-            </div>
-
-            </div>
-
 
             <div id="road">
                 <div id="stripes" ref={stripesRef}></div>
