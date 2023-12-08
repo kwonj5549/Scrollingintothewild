7f1899e292ac81a3a421ce6e96ca86e0adfddaac
kwill5800 <kwill5800@gmail.com>
2023-12-07 21:06:19 -0800

    Modify animation and styling for scroll influenced text
    This commit introduces enhanced styling and animation behaviors for scrolled text in App.js. Responsive 'textStyle' and 'displaySize' states have been added to adjust text display according to scroll position. Additionally, display thresholds are now customized per keyPoint to increase flexibility in animation behavior. Corresponding CSS updates are made for new styles and border detail. This enhances visual appeal and provides a more interactive scrolling experience.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import carImage from './assets/car.png';
 import backgroundImage from './assets/background.png';
 import mileMarker0 from './assets/mile0.png';
 import mileSign from './assets/distance_state_sign.png';
-import textDisplay1 from './assets/text_display.png';
+// import textDisplay1 from './assets/text_display.png';
 
 const App = () => {
     const mainTextRef = useRef(null);
@@ -23,7 +23,8 @@ const App = () => {
     const [displayText, setDisplayText] = useState("");
     const [displayDate, setDisplayDate] = useState("");
     const [textStyle, setTextStyle] = useState({ fontSize: '2.4rem', lineHeight: '1.6rem' });
-
+    const [displaySize, setDisplaySize] = useState({ width: '40vw', height: '30vh' });
+    const [movementThreshold, setMovementThreshold] = useState('30');
     const animateText = (y) => {
         gsap.to(mainTextRef.current, {
             y: -y * animationMultiplier,
@@ -89,7 +90,9 @@ const App = () => {
                 fontSize: currentKeyPoint.textSize || '2.4rem',
                 lineHeight: currentKeyPoint.textSpacing || '1.6rem'
             });
-            const displayThreshold = window.innerHeight * 0.39 / animationMultiplier + currentKeyPoint.start;
+            setDisplaySize(currentKeyPoint.displaySize || { width: '40vw', height: '30vh' });
+            setMovementThreshold(currentKeyPoint.movementThreshold || '30');
+            const displayThreshold = window.innerHeight * (movementThreshold/100) / animationMultiplier + currentKeyPoint.start;
             const offset = currentMiles - currentKeyPoint.end;
 
             if (currentMiles <= displayThreshold) {
@@ -97,6 +100,7 @@ const App = () => {
             } else if (currentMiles > displayThreshold) {
                 animateTextDisplay1(displayThreshold - currentKeyPoint.start);
             } else if (currentMiles > currentKeyPoint.end) {
+                console.log(newYPosition)
                 const newYPosition = (displayThreshold - currentKeyPoint.start) - offset / 4;
                 animateTextDisplay1(newYPosition);
             }
@@ -115,7 +119,8 @@ const App = () => {
                 "                        find a new sense of identity.",
             date: "June 1, 1990",
             textSize: "2.4rem", // Example text size
-            textSpacing: "2rem"
+            textSpacing: "2rem",
+      movementThreshold: '34'
         },
         {
             start: 130,
@@ -123,7 +128,9 @@ const App = () => {
             text: "I believe McCandless embarked on this journey to escape from the societal pressures and materialism he perceived in his life and upbringing particularly his parents, which contributed to his desire to seek independence and a new identity in the wild. Krakauer's \"Into the Wild\" suggests that McCandless was motivated by a mix of youthful idealism, a quest for raw, unfiltered experiences, and the influence of transcendentalist literature. His odyssey was as much about finding himself as it was about challenging his limits and shedding what he considered the unnecessary burdens of modern life.\n",
             date: "",
             textSize: "1.6rem", // Example text size
-            textSpacing: "1.2rem"
+            textSpacing: "1.2rem",
+            displaySize: { width: '40vw', height: '30vh' },
+            movementThreshold: '30',
         },
         {start: 300, end: 350, text: "When I started making this, I didn't realize that how long his journey was. So this might take a while", date: "",
             textSize: "2rem", // Example text size
@@ -136,8 +143,7 @@ const App = () => {
     const handleWheel = (event) => {
         event.preventDefault();
 
-        const normalizedDeltaY = Math.min(Math.max(event.deltaY, -1), 1);
-        const scrollIncrement = normalizedDeltaY;
+        const scrollIncrement = Math.min(Math.max(event.deltaY, -1), 1);
 
         animationCurrent.current += scrollIncrement;
 
@@ -197,8 +203,9 @@ const App = () => {
         return () => {
             window.removeEventListener("wheel", handleWheel);
         };
-    }, []);
 
+    }, );
+    const adjustedTop = `-${parseInt(displaySize.height, 10) + 1}vh`;
     return (
         <div className="App" style={{backgroundImage: `url(${backgroundImage})`}}>
             <div className="main-text" ref={mainTextRef}>
@@ -219,9 +226,14 @@ const App = () => {
                 </div>
             </div>
 
-            <div className="text-display-container" ref={textDisplay1Ref}>
-                <img src={textDisplay1} id="text-display-1" alt="Text Display 1"/>
-                <div className="display-text"style={{ fontSize: textStyle.fontSize, lineHeight: textStyle.lineHeight }}>
+            <div className="text-display-container" ref={textDisplay1Ref} style={{
+                top: adjustedTop,
+                width: displaySize.width,
+                height: displaySize.height,
+
+            }}>
+                {/*<img  id="text-display-1" alt="Text Display 1" style={{ width: '100%', height: '100%' }}/>*/}
+                <div className="display-text" style={{ fontSize: textStyle.fontSize, lineHeight: textStyle.lineHeight }}>
                     <div className="text-5xl">
                         <p>{displayDate}</p>
                     </div>
